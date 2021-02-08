be2bcf5473e5cf204863bac81ba50f7f81a35580
elaineyeh <10456018@ntub.edu.tw>
2021-02-08 10:40:44 +0800

    refactor: ssh to aws

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
 git pull
-$PROJECT_NAME=a
 
 mkdir docker-compose
-envsubst < docker-compose-template > docker-compose-$PROJECT_NAME.yml
-cp docker-compose-$PROJECT_NAME.yml ../docker-compose
+envsubst < docker-compose-template > docker-compose-$CIRCLE_PROJECT_REPONAME.yml
+cp docker-compose-$CIRCLE_PROJECT_REPONAME.yml ../docker-compose
+
+cd ../docker-compose
+FILENAME=`ls ./*.yml`
+FILE="docker-compose"
+for EACHFILE in $FILENAME
+do
+   $FILE+=" -f "+$EACHFILE
+done
+$FILE+=" up"
+
+sh -c $FILE
